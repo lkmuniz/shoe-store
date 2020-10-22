df4bce014ff416e6a4de1df806c180d2bcfe7415
Lucas Muniz <lucasmuniz@id.uff.br>
2020-10-22 14:08:48 -0300

    Add Data Binding

--- app/src/main/java/br/com/muniz/shoestore/MainActivity.kt
+++ app/src/main/java/br/com/muniz/shoestore/MainActivity.kt
@@ -2,13 +2,15 @@ package br.com.muniz.shoestore
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import br.com.muniz.shoestore.databinding.ActivityMainBinding
 import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
         Timber.plant(Timber.DebugTree())
     }
 }
