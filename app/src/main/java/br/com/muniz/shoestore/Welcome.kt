81f4f1b6d4e12f976de21428f619cf1e9e3e5d51
Lucas Muniz <lucasmuniz@id.uff.br>
2020-10-22 17:49:20 -0300

    Add welcome fragment

---
+++ app/src/main/java/br/com/muniz/shoestore/Welcome.kt
@@ -0,0 +1,27 @@
+package br.com.muniz.shoestore
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.fragment.findNavController
+import br.com.muniz.shoestore.databinding.FragmentWelcomeBinding
+
+
+class Welcome : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val binding = DataBindingUtil.inflate<FragmentWelcomeBinding>(
+                inflater, R.layout.fragment_welcome, container, false)
+
+
+        return binding.root
+    }
+
+}
