f28739d971fe85c0f3c1bc406dec4922235c4e1a
Lucas Muniz <lucasmuniz@id.uff.br>
2020-10-23 18:39:28 -0300

    Add listview fragment

--- app/src/main/java/br/com/muniz/shoestore/shoeList/ShoeList.kt
+++ app/src/main/java/br/com/muniz/shoestore/shoeList/ShoeList.kt
@@ -6,10 +6,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import br.com.muniz.shoestore.R
 import br.com.muniz.shoestore.databinding.FragmentShoeListBinding
+import kotlinx.android.synthetic.main.shoe_item_list_layout.view.*
 
 class ShoeList : Fragment() {
+
+    private lateinit var viewModel: ShoeListViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -19,6 +24,20 @@ class ShoeList : Fragment() {
         val binding = DataBindingUtil.inflate<FragmentShoeListBinding>(
             inflater, R.layout.fragment_shoe_list, container, false
         )
+
+        // get the viewModel from ViewModelProvider
+        viewModel = ViewModelProvider(this).get(ShoeListViewModel::class.java)
+
+        // a observer to create populate the scrollview in any change
+        viewModel.shoeList.observe(viewLifecycleOwner, Observer { newShoe ->
+            newShoe.forEach {
+                var view = View.inflate(context, R.layout.shoe_item_list_layout, null)
+                view.shoe_item_name.text = it.name
+                view.shoe_item_company.text = it.company
+                view.shoe_item_size.text = it.size.toString()
+                binding.shoeItemLayout.addView(view)
+            }
+        })
 
         return binding.root
     }
--- app/src/main/java/br/com/muniz/shoestore/shoeList/ShoeListViewModel.kt
+++ app/src/main/java/br/com/muniz/shoestore/shoeList/ShoeListViewModel.kt
@@ -1,7 +1,30 @@
 package br.com.muniz.shoestore.shoeList
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import br.com.muniz.shoestore.models.Shoe
 
-class ShoeListViewModel : ViewModel(){
+class ShoeListViewModel : ViewModel() {
 
+    private val _shoeList = MutableLiveData<List<Shoe>>()
+    val shoeList: LiveData<List<Shoe>>
+        get() = _shoeList
+
+    init {
+        fillShoeList()
+    }
+
+    private fun fillShoeList() {
+        _shoeList.value = listOf(
+            Shoe("Nike Shoks", 35.0, "Nike", "Nike SHoks", mutableListOf("image1")),
+            Shoe("Puma ", 36.0, "Puma", "Puma", mutableListOf("image2")),
+            Shoe("Olympikus ", 37.0, "Olympikus", "Olympikus", mutableListOf("image2")),
+            Shoe("Adidas ", 38.0, "Adidas", "Adidas", mutableListOf("image2")),
+            Shoe("Mizuno ", 39.0, "Mizuno", "Mizuno", mutableListOf("image2")),
+            Shoe("Topper ", 40.0, "Topper", "Topper", mutableListOf("image2")),
+            Shoe("Reebok ", 41.0, "Reebok", "Reebok", mutableListOf("image2")),
+            Shoe("Umbro ", 42.0, "Umbro", "Umbro", mutableListOf("image2"))
+        )
+    }
 }
