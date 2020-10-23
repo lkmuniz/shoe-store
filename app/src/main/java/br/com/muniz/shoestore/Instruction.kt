883c6e257a16fb6c4b8708bdc7414bf6a4cceb50
Lucas Muniz <lucasmuniz@id.uff.br>
2020-10-23 09:42:50 -0300

    Add instruction fragment

---
+++ app/src/main/java/br/com/muniz/shoestore/Instruction.kt
@@ -0,0 +1,25 @@
+package br.com.muniz.shoestore
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import br.com.muniz.shoestore.databinding.FragmentInstructionBinding
+
+class Instruction : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val binding = DataBindingUtil.inflate<FragmentInstructionBinding>(
+            inflater, R.layout.fragment_instruction, container, false
+        )
+
+        return binding.root
+    }
+
+}
