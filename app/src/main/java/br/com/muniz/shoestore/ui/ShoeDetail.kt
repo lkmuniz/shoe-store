33745adb2fd827c44b916d11ee2959a1ffeb2206
Lucas Muniz <lucasmuniz@id.uff.br>
2020-10-25 21:36:45 -0300

    Refactor Detail fragment

--- app/src/main/java/br/com/muniz/shoestore/ui/ShoeDetail.kt
+++ app/src/main/java/br/com/muniz/shoestore/ui/ShoeDetail.kt
@@ -1,16 +1,15 @@
-package br.com.muniz.shoestore.shoeDetails
+package br.com.muniz.shoestore.ui
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.databinding.DataBindingUtil
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import br.com.muniz.shoestore.R
 import br.com.muniz.shoestore.databinding.FragmentShoeDetailsBinding
+import br.com.muniz.shoestore.shoeDetails.ShoeDetailViewModel
 
 class ShoeDetail : Fragment() {
 
